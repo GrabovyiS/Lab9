8b05fd83593ce8b5806aff7d9c991610f3d2d07c
Saveliy Graboviy$ git config --global user.name Saveliy <graboviys@yandex.ru>
2020-12-18 12:38:01 +0300

    the end

--- 3.js
+++ 3.js
@@ -1,19 +1,18 @@
+'use strict'
+let flag;
 function div2(val, by){
   return (val - val%by)/by;
 }
-
 function isPalindrome(word){
   flag = true;
-  let i = div2(word.lenght, 2);
-  for (i-1; i>=0; i--){
-    if(word[i] !== word[word.lenght-1-i]){
+  let i = div2(word.length, 2) - 1;
+  for (; i>=0; i--){
+    if(word[i] != word[word.length-1-i]){
       flag = false;
     }
-  } 
-  if (flag = false){
-    return false;
-  } else {return true};
+  }
+  return flag;
 }
 console.log(isPalindrome('довод'));
-console.log(isPalindrome('фыва'));
-console.log(isPalindrome('абба'));
+console.log(isPalindrome('фыыф'));
+console.log(isPalindrome('кружка'));
---
+++ 4.js
@@ -0,0 +1,9 @@
+function scalarMultiplication(matrix,multiplier){
+  for (let i = 0; i<matrix.length; i++){
+    for (let j = 0; j<matrix[i].length;j++){
+      matrix[i][j]*= multiplier
+    }
+  }
+  console.log(matrix)
+}
+scalarMultiplication([[1,2,3],[4,5,6],[7,8,9]],3)
